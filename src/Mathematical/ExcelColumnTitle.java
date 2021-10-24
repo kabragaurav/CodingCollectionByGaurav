c0fc86699a93cfeb2c514b2c63d9a8c33b10fb65
Gaurav Kabra <gauravkabra.official@gmail.com>
2021-10-24 10:17:27 +0530

    24 Oct 2021

---
+++ src/Mathematical/ExcelColumnTitle.java
@@ -0,0 +1,33 @@
+/**
+ * Given an integer columnNumber,
+ * return its corresponding column title as it appears in an Excel sheet.
+ * E.g. If n = 1, return "A". If n = 28, return "AB".
+ */
+package Mathematical;
+
+/**
+ * @author gaurav kabra
+ * @since 24 Oct 2021
+ **/
+
+public class ExcelColumnTitle {
+
+    private static String convertToTitle(int n) {
+        String ans = "";
+        while(n > 0) {                      // while n is not 0
+            n--;
+            ans += (char) ('A' + n%26);
+            n /= 26;
+        }
+
+        return new StringBuilder(ans).reverse().toString();
+    }
+
+    // driver - main method
+    public static void main(String[] args) {
+        // TESTCASES
+        System.out.println(convertToTitle(1));
+        System.out.println(convertToTitle(701));
+        System.out.println(convertToTitle(666));
+    }
+}
---
+++ src/Searching/FirstAndLastIndices.java
@@ -0,0 +1,78 @@
+/**
+ * Given an array of integers nums sorted in non-decreasing order,
+ * find the starting and ending position of a given target value.
+ * If target is not found in the array, return [-1, -1].
+ */
+package Searching;
+
+/**
+ * @author gaurav kabra
+ * @since 24 Oct 2021
+ **/
+
+import java.util.Arrays;
+
+public class FirstAndLastIndices {
+
+    private static int findFirstIndex(int[] nums, int target) {
+        int index = -1;
+        int start = 0, end = nums.length-1, mid;
+        while(start <= end) {
+            mid = start + (end-start)/2;
+            if(nums[mid] == target) {
+                index = mid;
+            }
+            if(nums[mid] < target) {            // this cannot be else if
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return index;
+    }
+
+    private static int findLastIndex(int[] nums, int target) {
+        int index = -1;
+        int start = 0, end = nums.length-1, mid;
+        while(start <= end) {
+            mid = start + (end-start)/2;
+            if(nums[mid] == target) {
+                index = mid;
+            }
+            if(nums[mid] <= target) {         // only difference from above method
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Logic:
+     * Apply binary search since array is sorted.
+     *
+     * TC : O(logN)
+     * SC : O(1)
+     */
+    private static int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[2];
+        ans[0] = findFirstIndex(nums, target);
+        ans[1] = findLastIndex(nums, target);
+        return ans;
+    }
+
+    // helper method to print int[]
+    private static void print(int[] searchRange) {
+        // convert int[] first to Integer[] then print
+        System.out.println(Arrays.deepToString(Arrays.stream(searchRange).boxed().toArray(Integer[]::new)));
+    }
+
+    // driver - main method
+    public static void main(String[] args) {
+        // TESTCASES
+        print(searchRange(new int[] {1,4}, 4));
+        print(searchRange(new int[] {}, 0));
+        print(searchRange(new int[] {1,2,2}, 2));
+    }
+}
