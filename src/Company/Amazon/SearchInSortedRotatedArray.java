bb393ba9031844cc68404ce6650abc7a7dbc789b
Gaurav Kabra <gauravkabra.official@gmail.com>
2022-02-02 18:22:22 +0530

    02 Feb 2022

--- src/Company/Amazon/SearchInSortedRotatedArray.java
+++ src/Company/Amazon/SearchInSortedRotatedArray.java
@@ -1,3 +1,13 @@
+/**
+ * Pramp
+ * A sorted array of distinct integers shiftArr is shifted to the left by an unknown offset
+ * and you don’t have a pre-shifted copy of it. For instance,
+ * the sequence 1, 2, 3, 4, 5 becomes 3, 4, 5, 1, 2, after shifting it twice to the left.
+ * Given shiftArr and an integer num, implement a function shiftedArrSearch that
+ * finds and returns the index of num in shiftArr. If num isn’t in shiftArr, return -1.
+ * Assume that the offset can be any value between 0 and arr.length - 1.
+ * Explain your solution and analyze its time and space complexities.
+ */
 package Company.Amazon;
 
 /**
@@ -7,6 +17,8 @@ package Company.Amazon;
 
 public class SearchInSortedRotatedArray {
 
+    // TC : O(logN)
+    // SC : O(1)
     private static int search(int[] nums, int target) {
         int N = nums.length;
         int left = 0;
@@ -16,13 +28,14 @@ public class SearchInSortedRotatedArray {
             if(nums[mid] == target) {
                 return mid;
             }
-            if(nums[left] <= nums[mid]) {        // left part sorted
-                if(nums[left] <= target && target <= nums[mid]) {
+            // can never be the case that both parts are unsorted.
+            if(nums[left] <= nums[mid]) {        // check if left part sorted
+                if(nums[left] <= target && target <= nums[mid]) {   // is number in this?
                     right = mid-1;
                 } else {
                     left = mid+1;
                 }
-            } else {
+            } else {        // then right one is sorted
                 if(nums[mid] <= target && target <= nums[right]) {
                     left = mid+1;
                 } else {
