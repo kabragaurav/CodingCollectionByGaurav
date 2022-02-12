9f89c85f0486a88ba203477f65f433fbb22c8d76
Gaurav Kabra <gauravkabra.official@gmail.com>
2022-02-12 20:41:59 +0530

    12 Feb 2022

---
+++ src/Heaps/KthSmallestInSortedMatrix.java
@@ -0,0 +1,48 @@
+/**
+ * Given an n x n matrix where each of the rows and columns is sorted in ascending order,
+ * return the kth smallest element in the matrix.
+ * Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+ */
+package Heaps;
+
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
+/**
+ * @author gkabra
+ * @since 12-02-2022 Sat
+ **/
+
+public class KthSmallestInSortedMatrix {
+
+    /**
+     * TC :
+     *  offer() is O(logN)
+     *  poll() is O(logN)
+     *  So, total is O(log(M*N)) + O(k*log(M*N)) = k*O(logMN)
+     *
+     * SC : O(M*N)
+     */
+    private static int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Arrays.stream(matrix)
+                .flatMapToInt(x -> Arrays.stream(x))
+                .forEach(a -> pq.offer(a));
+
+
+        for(int i=0; i<k-1; i++) {
+            pq.poll();
+        }
+        return pq.poll();
+    }
+
+    // driver - main method
+    public static void main(String[] args) {
+        // TESTCASE
+        System.out.println(kthSmallest(new int[][] {
+                {1,2},
+                {1,3}
+        }, 2));
+    }
+
+}
