3042a349c4212c1db15854432521e758a537efb5
gauravkabra <gauravkabra.official@gmail.com>
2022-04-13 11:58:22 +0530

    13 April 2022
    Signed-off-by: gauravkabra <gauravkabra.official@gmail.com>

---
+++ src/Company/Microsoft/OOD/BuilderDesignPattern/MainBuilder.java
@@ -0,0 +1,26 @@
+/*
+    Builder Design Pattern falls under Creational Design Pattern.
+
+    Basically, instead of ctor, we use setters.
+    This allows us to skip few properties while creating objects.
+ */
+package Company.Microsoft.OOD.BuilderDesignPattern;
+
+/**
+ * @author gaurav kabra
+ * @since 13/Apr/2022
+ **/
+
+public class MainBuilder {
+
+    // driver - main method
+    public static void main(String[] args) {
+        // TESTCASE(S)
+        Phone phone = new Phone().setName("gaurav's iphone").setOs("ios");
+        System.out.println(phone);
+
+        phone = new Phone().setOs("Android").setName("gaurav's realme").setProcessor("qualcom");
+        System.out.println(phone);
+    }
+
+}
